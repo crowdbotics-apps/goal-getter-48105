44d2e4e51781030a2ced1513e135226f5373d9c6
Crowdbotics <team@crowdbotics.com>
2024-05-26 04:58:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "goal-getter-48105",
+  url: "https://goal-getter-48105.botics.co",
+  api: "https://goal-getter-48105.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
